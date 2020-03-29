da2e01d4245d6f99138b4d34f51ca87266d45577
Zhidan Luo <zhidanluo007@gmail.com>
2020-03-28 21:01:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-import utils
+from utils import BCNF, checkFile
 import os
+import _config
 
-# change this path to your input folder
-# do NOT add txt files that is not a valid input
-fileFolder = "./input/"
+inputFileFolder = _config.inputFileFolder
+specify = _config.specify
 
-for file in sorted(os.listdir(fileFolder)):
-    if file[-4:] == '.txt':
-        fileName = fileFolder + file
-        utils.printSol(fileName)
+# make input folder
+if not os.path.exists(inputFileFolder):
+    os.makedirs(inputFileFolder)
+    print("No input file found! Please add input file under '%s'!" % inputFileFolder)
+    os._exit(0)
+
+# make save folder
+outputFileFolder = "./output/"
+if not os.path.exists(outputFileFolder):
+    os.makedirs(outputFileFolder)
+
+# check input folder
+files = sorted(os.listdir(inputFileFolder))
+num_txtFiles = len([file for file in files if file[-4:] == ".txt"])
+if num_txtFiles == 0:
+    print("No input file found! Please add input file under '%s'!" % inputFileFolder)
+    os._exit(0)
+
+# begin calculating BCNF
+if not specify:
+    for file in sorted(os.listdir(inputFileFolder)):
+        if file != '.DS_Store':
+            checkFile(file)
+    for file in sorted(os.listdir(inputFileFolder)):
+        BCNF(file)
+else:
+    file = specify
+    if not os.path.exists(inputFileFolder+file):
+        print("No input file named '%s'!" % file)
+        os._exit(0)
+    checkFile(file)
+    BCNF(file)
