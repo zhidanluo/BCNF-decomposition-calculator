a5a2ef5fdbb4a96672b8849c3557fe1c4dfff069
Zhidan Luo <zhidanluo007@gmail.com>
2020-03-28 21:02:38 -0400

    Add files via upload

---
+++ _config.py
@@ -0,0 +1,14 @@
+# change this path to your input folder (input file should be *.txt)
+inputFileFolder = "./input/"
+
+# wether to save solutions
+save = True
+
+# wether to show information
+printf = True
+
+# wether to show F+ (only works if printf = True)
+printFplus = True
+
+# specify one particular file to process or all files if set to None 
+specify = None
